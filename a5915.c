4e518c000a0cc6cf7dbf880ecc912ddb2b31b657
Lauren Escobedo <lescobedo4@mail.stmarytx.edu>
2021-11-04 22:02:38 +0000

    a5915.c fix

--- a5915.c
+++ a5915.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 #include <stdbool.h>
-double median(double x, double y, double z)
+
+double median(double x, double y, double z);
+
 int main()
 {
-	int value;
+	double a, b, c;
+	a = 1;
+	b = 2;
+	c = 3;
+	
+	printf("%f", median(a, b, c));
+	return 0;
+}
+
+double median(double x, double y, double z)
+{
+	double value;
 	if(x<=y)
 	{
 		if(y<=z)
 		{
 			value = y;
 		}
-		else{
+		else
+		{
 			value = z;
 		}
 	}
