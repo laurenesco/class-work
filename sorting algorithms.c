03ee3c395bddca0d5912dc2cf4fba0580ad36b49
Lauren Escobedo <lescobedo4@mail.stmarytx.edu>
2021-11-30 05:36:04 +0000

    Testing sorting algorithms bug fix

--- sorting algorithms.c
+++ sorting algorithms.c
@@ -105,7 +105,7 @@ int main()
 	bubble_sort(s, SMALL_ARRAY);
 	a = clock() - a;
 	double time_taken1 = ((double)a)/CLOCKS_PER_SEC;
-	printf("Bubble sort on %d elements took %f seconds to execute.", SMALL_ARRAY, time_taken1);
+	printf("Bubble sort on %d elements took %f seconds to execute.\n", SMALL_ARRAY, time_taken1);
 
 	//bubblesort medium
 	clock_t b;
@@ -113,7 +113,7 @@ int main()
 	bubble_sort(m, MEDIUM_ARRAY);
 	b = clock() - b;
 	double time_taken2 = ((double)b)/CLOCKS_PER_SEC;
-	printf("Bubble sort on %d elements took %f seconds to execute.", SMALL_ARRAY, time_taken2);
+	printf("Bubble sort on %d elements took %f seconds to execute.\n", MEDIUM_ARRAY, time_taken2);
 	
 	//bubblesort large 
 	clock_t c;
@@ -121,7 +121,7 @@ int main()
 	bubble_sort(l, LARGE_ARRAY);
 	c = clock() - c;
 	double time_taken3 = ((double)c)/CLOCKS_PER_SEC;
-	printf("Bubble sort on %d elements took %f seconds to execute.", SMALL_ARRAY, time_taken3);
+	printf("Bubble sort on %d elements took %f seconds to execute.\n", LARGE_ARRAY, time_taken3);
 	
 	//selectionsort small
 	clock_t d;
@@ -129,7 +129,7 @@ int main()
 	selection_sort(s, SMALL_ARRAY);
 	d = clock() - d;
 	double time_taken4 = ((double)d)/CLOCKS_PER_SEC;
-	printf("Bubble sort on %d elements took %f seconds to execute.", SMALL_ARRAY, time_taken4);
+	printf("Selection sort on %d elements took %f seconds to execute.\n", SMALL_ARRAY, time_taken4);
 	
 	//selectionsort medium
 	clock_t e;
@@ -137,7 +137,7 @@ int main()
 	selection_sort(m, MEDIUM_ARRAY);
 	e = clock() - e;
 	double time_taken5 = ((double)e)/CLOCKS_PER_SEC;
-	printf("Bubble sort on %d elements took %f seconds to execute.", SMALL_ARRAY, time_taken5);
+	printf("Selection sort on %d elements took %f seconds to execute.\n", MEDIUM_ARRAY, time_taken5);
 	
 	//selectionsort large
 	clock_t f;
@@ -145,7 +145,7 @@ int main()
 	selection_sort(l, LARGE_ARRAY);
 	f = clock() - f;
 	double time_taken6 = ((double)f)/CLOCKS_PER_SEC;
-	printf("Bubble sort on %d elements took %f seconds to execute.", SMALL_ARRAY, time_taken6);
+	printf("Selection sort on %d elements took %f seconds to execute.\n", LARGE_ARRAY, time_taken6);
 	
 	//quicksort small
 	clock_t g;
@@ -153,7 +153,7 @@ int main()
 	quick_sort(s, 0, SMALL_ARRAY);
 	g = clock() - g;
 	double time_taken7 = ((double)g)/CLOCKS_PER_SEC;
-	printf("Bubble sort on %d elements took %f seconds to execute.", SMALL_ARRAY, time_taken7);
+	printf("Quick sort on %d elements took %f seconds to execute.\n", SMALL_ARRAY, time_taken7);
 	
 	//quicksort medium
 	clock_t h;
@@ -161,7 +161,7 @@ int main()
 	quick_sort(m, 0, MEDIUM_ARRAY);
 	h = clock() - h;
 	double time_taken8 = ((double)h)/CLOCKS_PER_SEC;
-	printf("Bubble sort on %d elements took %f seconds to execute.", SMALL_ARRAY, time_taken8);
+	printf("Quick sort on %d elements took %f seconds to execute.\n", MEDIUM_ARRAY, time_taken8);
 	
 	//quicksort large
 	clock_t i;
@@ -169,7 +169,7 @@ int main()
 	quick_sort(l, 0, LARGE_ARRAY);
 	i = clock() - i;
 	double time_taken9 = ((double)i)/CLOCKS_PER_SEC;
-	printf("Bubble sort on %d elements took %f seconds to execute.", SMALL_ARRAY, time_taken9);
+	printf("Quick sort on %d elements took %f seconds to execute.\n", LARGE_ARRAY, time_taken9);
 	
 }
 
